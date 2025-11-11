a677243d25e9208e0c3657758f832180c11d4cbd
Douglastm <douglasteixeiramagalhaes@gmail.com>
2025-11-10 23:11:22 -0300

    first commit

---
+++ src/main/java/com/dev/schoolbox/SchoolboxApplication.java
@@ -0,0 +1,13 @@
+package com.dev.schoolbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/schoolbox/SchoolboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.schoolbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
